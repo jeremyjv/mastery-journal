022091b64c91e1a91f5a92298ec42dff572cd9f4
Jeremy Villanueva <jeremyjvillanueva@gmail.com>
2024-01-09 19:54:45 -0800

    rendered goals

--- frontend/src/components/goals.component.js
+++ frontend/src/components/goals.component.js
@@ -1,41 +1,59 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+
 //A goal has one id 
 //this component can be purely visual and we update 
 // all backend info in journal 
-export default class Goals extends Component {
+
+
+    
+export default class Goals extends React.Component {
+    
+    
     constructor(props) {
         super(props);
-
         this.state = {
             goals: [],
-            test: ''
+            skills: []
 
+           
         }
+        
     }
 
     componentDidMount() {
         axios.get('http://localhost:4000/journal/65942e93c325434c995eaa4b')
-            .then(response => {
-                if (response.data.length) {
-                    this.setState({
-                
-                        test: response.data.goals[0]
-                    })
-                }
+            .then(res => {
+                this.setState({
+                    goals: res.data.goals,
+                    skills: res.data.skills
+                })
             })
+                
+         
+            
+        console.log(this.state.goals);
     }
-   
-
+    
+    
     render() {
+        
         return (
-            <div className='text-white font-body text-center'>
-                Goals: [ 10 Percent Body Fat ][ 2024 Internship ][ 10000 Dollar Sales ]
-                <div className='text-white'>{this.test}</div>
-            </div>
-        )
-    }
+            <div>
+                <div className='text-white font-body text-center'>
+                    Goals: { this.state.goals.map(item => `[ ${item} ]`) }
+                   
+                </div>
 
+                <div className='text-white font-body text-center'>
+            
+                    Skills: { this.state.skills.map(item => `[ ${item} ]`) } 
+                </div>
+            </div>
+            
+        );
+    }
 
 }
+export { Goals };
